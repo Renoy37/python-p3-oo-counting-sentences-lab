72394ee72ae4d6cfcba07cbab7e28ca767b63fe8
Robins <robinmgk077@gmail.com>
2024-03-02 14:28:47 +0300

    Completed Assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,47 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        if self.value[-1] == ".":
+            return True
+        else:
+            return False
+
+    def is_question(self):
+        if self.value.endswith("?"):
+            return True
+        else:
+            return False
+
+    def is_exclamation(self):
+        if self.value.endswith("!"):
+            return True
+        else:
+            return False
+
+    def count_sentences(self):
+        punctuation = ".?!"
+
+        count = 0
+        start_index = 0
+
+        for i, char in enumerate(self.value):
+            if char in punctuation:
+                if i == len(self.value) - 1 or self.value[i + 1] not in punctuation:
+                    count += 1
+
+        return count
